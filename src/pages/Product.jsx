688b1f58bdf3245798258e024ee73e972826846b
Pooran Sen <pooransen50@gmail.com>
2022-05-14 19:44:22 +0530

    Product Page Created

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,140 @@
+import { Add, Remove } from "@mui/icons-material";
 import styled from "styled-components"
+import Anouncement from "../components/Anouncement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 50vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    margin-top: 10px;
+`;
+
+const Filter = styled.div``;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border:1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid black;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
+
+
 
 const Product = () => {
   return (
     <Container>
-        hello
+        <Anouncement/>
+        <Navbar/>
+
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/a/_/a_502.png"/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>RTX 3060</Title>
+                <Desc>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquam cum distinctio optio sint voluptatibus quaerat voluptatem similique sit, eligendi id magni vero accusantium corporis consequuntur ab eum iure doloribus?
+                </Desc>
+                <Price>$20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                        <FilterSizeOption>4GB</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+
+                <AddContainer>
+                    <AmountContainer>
+                        <Remove/>
+                        <Amount>1</Amount>
+                        <Add/>
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
+
+            </InfoContainer>
+        </Wrapper>
+
+        <Newsletter/>
+        <Footer/>
     </Container>
   )
 }
