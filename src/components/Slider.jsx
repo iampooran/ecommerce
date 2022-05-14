d39763dca30b2788f4fed790dae29f10dd950175
Pooran Sen <pooransen50@gmail.com>
2022-05-14 18:01:55 +0530

    Newsletter and Product Added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,6 +34,7 @@ const Arrow  = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
+    transition:all 1.5s ease;
     transform:translateX(${props=>props.slideIndex * -100}vw);
 `;
 
@@ -100,7 +101,7 @@ const Slider = () => {
 
         <Wrapper slideIndex={slideIndex}>
             {sliderItems.map(item=>(
-            <Slide bg={item.bg}>
+            <Slide bg={item.bg} key={item.id}>
             <ImgContainer>
                 <Image src={item.img}/>
             </ImgContainer>
--- src/data.js
+++ src/data.js
@@ -22,3 +22,74 @@ export const sliderItems =[
     }
     
 ];
+
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1597872200969-2b65d56bd16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+        title: "Graphic Card",
+    },
+
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1587202372775-e229f172b9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+        title: "Cabinet",
+    },
+
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1611275484845-52a71f2b0a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+        title: "Processor",
+    },
+    
+];
+
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_492.png",
+    },
+
+    {
+        id: 2,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/a/_/a_502.png",
+    },
+    {
+        id: 3,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_490_2.png",
+    },
+
+    {
+        id: 4,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_478_1.png",
+    },
+    {
+        id: 5,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_ready_332.png",
+    },
+    {
+        id: 6,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_ready_331.png",
+    },
+    {
+        id: 7,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_ready_328.png",
+    },
+    {
+        id: 8,
+        img: "https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/b/_/b_ready_330.png",
+    },
+    {
+        id:9,
+        img:"https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/a/_/a_47.png",
+    },
+    {
+        id:10,
+        img:"https://www.bitkart.com/pub/media/catalog/product/cache/0ee050c3ffc3555709b9bb6062f4d7e9/c/_/c_429.png",
+    },
+];
+
+
